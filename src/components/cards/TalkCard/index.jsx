7bc7f292e30c28443b91b5e44ef4b9ff1df98893
Alejandro M. Coca <alxmcr@outlook.com>
2021-05-06 19:38:08 -0400

    Working with the Schedule

--- src/components/cards/TalkCard/index.jsx
+++ src/components/cards/TalkCard/index.jsx
@@ -4,11 +4,13 @@ export function TalkCard({ talk }) {
     const { id, time, title, speakerName, speakerPhoto } = talk;
     return (
         <div id={id} className={styles.TalkCard}>
-            <span className={styles.TalkTime}>{time}</span>
-            <img className={styles.TalkSpeakerPhoto}
-                src={speakerPhoto}
-                alt={speakerName}
-            />
+            <div className={styles.TalkDetails}>
+                <span className={styles.TalkTime}>{time}</span>
+                <img className={styles.TalkSpeakerPhoto}
+                    src={speakerPhoto}
+                    alt={speakerName}
+                />
+            </div>
             <div className={styles.TalkSpeakerInfo}>
                 <span className={styles.TalkSpeakerName}>{speakerName}</span>
                 <h3 className={styles.TalkTitle}>{title}</h3>
