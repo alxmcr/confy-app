d651805dc00034d020361ab4d2be6a42e797a588
Alejandro M. Coca <alxmcr@outlook.com>
2021-05-06 17:46:42 -0400

    Hero

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,7 +1,10 @@
+import { HeroSection } from 'sections/HeroSection'
 import styles from './HomePage.module.css'
 
 export function HomePage() {
     return (
-        <div className={styles.HomePage}></div>
+        <div className={styles.HomePage}>
+            <HeroSection />
+        </div>
     )
 }
--- src/sections/HeroSection/index.jsx
+++ src/sections/HeroSection/index.jsx
@@ -2,8 +2,17 @@ import styles from './HeroSection.module.css'
 
 export function HeroSection() {
     return (
-        <section>
-
+        <section id="hero" className={styles.HeroSection}>
+            <div className={styles.heroSectionBody}>
+                <h1 className={styles.heroSectionTitle}>Confy</h1>
+                <p className={styles.heroSectionDescription}>Online Tech Conference <span className={styles.heroSectionDate}>October 4th.</span></p>
+                <p className={styles.heroSectionLocation}>París, France</p>
+            </div>
+            <div className={styles.HeroSectionActions}>
+                <a href="#tickets"
+                    className={styles.HeroSectionLinkCTA}
+                >Tickets</a>
+            </div>
         </section>
     )
 }
