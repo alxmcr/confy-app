3095feba0dcb55d27abea5bcad2e782b078465c2
Alejandro M. Coca <alxmcr@outlook.com>
2021-05-06 21:42:11 -0400

    Working with scroll

--- src/sections/TicketsSection/index.jsx
+++ src/sections/TicketsSection/index.jsx
@@ -3,7 +3,7 @@ import styles from './TicketsSection.module.css'
 
 export function TicketsSection() {
     return (
-        <section className={styles.TicketsSection}>
+        <section id="tickets" className={styles.TicketsSection}>
             <h2 className={styles.TicketsSectionTitle}>Your ticket to Confy 2021</h2>
             <TicketPaymentCard />
         </section>
